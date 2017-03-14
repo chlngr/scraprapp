778c656d3ce3476fa6f63f82102ad9abe0dabf91
Wolf <Wolf@Wolfs-MacBook-Air.local>
2017-03-14 19:15:16 -0400

    rejigger the app to have a single page waiting on a listener. not pulling the location on my phone unfortunately. click a button to save location, which should show on the home screen.

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -3,57 +3,70 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+// include requires ionic and ngCordova
+angular.module('starter', ['ionic', 'ngCordova'])
 
-.controller('AppCtrl', function($scope, $ionicModal) {
+.controller('AppCtrl', function($scope, $ionicModal, $cordovaGeolocation, $ionicLoading, $ionicPlatform) {
 
 //initialize the scope variables at 0
   $scope.latitude = 0;
   $scope.longitude = 0;
 
-  $ionicModal.fromTemplateUrl('location-request.html', function(modal) {
-    $scope.locationModal = modal;
-  },  {
-    scope: $scope,
-    animation: 'slide-in-up'
-  });
 
 
-  $scope.newSearch = function() {
-    $scope.locationModal.show();
-  };
-
-  $scope.cancelSearch = function() {
-    $scope.locationModal.hide();
-  };
-
-  $scope.runSearch = function(latitude, longitude) {
-    $scope.latitude = latitude;
-    $scope.longitude = longitude;
-    $scope.locationModal.hide();
-
-  }
+  // $ionicModal.fromTemplateUrl('location-request.html', function(modal) {
+  //   $scope.locationModal = modal;
+  // },  {
+  //   scope: $scope,
+  //   animation: 'slide-in-up'
+  // });
 
 
+  // $scope.newSearch = function() {
+  //   $scope.locationModal.show();
+  // };
 
-  }
-  )
+  // $scope.cancelSearch = function() {
+  //   $scope.locationModal.hide();
+  // };
 
+$ionicPlatform.ready( function() {
+      $ionicLoading.show({
+        template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+    });
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    var posOptions = {
+      enableHighAccuracy: true,
+      timeout: 20000,
+      maximumAge: 0
+      };
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+    $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+
+      }); 
+    });
+$scope.saveLocation = function() {
+    $scope.latitude  = position.coords.latitude;
+    $scope.longitude = position.coords.longitude;
+};
+
 })
+
+
+// .run(function($ionicPlatform) {
+//   $ionicPlatform.ready(function() {
+//     if(window.cordova && window.cordova.plugins.Keyboard) {
+//       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+//       // for form inputs)
+//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+//       // Don't remove this line unless you know what you are doing. It stops the viewport
+//       // from snapping when text inputs are focused. Ionic handles this internally for
+//       // a much nicer keyboard experience.
+//       cordova.plugins.Keyboard.disableScroll(true);
+//     }
+//     if(window.StatusBar) {
+//       StatusBar.styleDefault();
+//     }
+//   });
+// });
